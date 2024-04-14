40d13a59fab5c4897781461d3d6a7a245f155689
ColinJV <colinjamesvandyke@gmail.com>
2024-04-13 22:27:12 -0700

    Created Card classes

---
+++ Pazaak/MainCard.cpp
@@ -0,0 +1,36 @@
+#include "MainCard.hpp"
+
+/********************************************************************************************************
+* Function: MainCard destructor																			*
+* Date Created: 4/13/2024																				*
+* Date Last Modified: 4/13/2024																			*
+* Programmer: Colin Van Dyke																			*
+* Description: Destroys a MainCard object.																*
+* Input parameters: void																				*
+* Returns: void																							*
+* Preconditions: None																					*
+* Postconditions: None																					*
+********************************************************************************************************/
+
+MainCard::~MainCard() {
+	std::cout << "Inside MainCard destructor." << std::endl;
+}
+
+
+/********************************************************************************************************
+* Function: displayCard()																				*
+* Date Created: 4/13/2024																				*
+* Date Last Modified: 4/13/2024																			*
+* Programmer: Colin Van Dyke																			*
+* Description: Draws a MainCard object in a window.														*
+* Input parameters: RenderWindow& window, a reference to a RenderWindow in which the MainCard is being	*
+* drawn.																								*
+* Returns: void																							*
+* Preconditions: None																					*
+* Postconditions: None																					*
+********************************************************************************************************/
+
+void MainCard::displayCard(RenderWindow& window) {
+	window.draw(this->mCard);
+	// will need to print a text box on the card to indicate its value
+}
