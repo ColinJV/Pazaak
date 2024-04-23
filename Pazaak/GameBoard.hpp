7833acd360489bf32e1005f6f4adb548a5ab7f18
CB <lyn.v.xx@gmail.com>
2024-04-22 17:55:33 -0700

    Updated GameBoard.hpp and GameBoard.cpp to include textures. Added textures to the cards and adjusted the position of card texts in the Card.cpp/Card.hpp class. Updated Match::displayMatch() to update and redisplay current scores.

--- Pazaak/GameBoard.hpp
+++ Pazaak/GameBoard.hpp
@@ -23,15 +23,18 @@ public:
 	vector<RectangleShape> getPlayerHand();
 	void initCards();
 	void initIndicators();
+	void initScoreCard();
 
 	void display(RenderWindow& window);
 
 	void setPlayerIndicator(int score);
 	void setBotIndicator(int score);
 	void setTurnIndicator(int player);
+	void setPlayerScore(int score);
+	void setBotScore(int score);
 	Vector2f getBotCardPosition(int card);
 	Vector2f getPlayerCardPosition(int card);
-	
+
 private:
 	vector<RectangleShape> playerHand;
 	vector<RectangleShape> botHand;
@@ -45,4 +48,12 @@ private:
 	Text turnIndicatorText;
 	Text playerText;
 	Text botText;
+	RectangleShape scoreCard;
+	Text botScore;
+	Text playerScore;
+	Text scoreCardLabels;
+	sf::Texture scoreCard_texture;
+	sf::Texture background_texture;
+	sf::Texture scoreIndicator_texture;
+	sf::Texture cardSkin;
 };
